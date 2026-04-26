836c31ac47999d7f1095944896001679a0256fc4
THmj26 <2858343380@qq.com>
2026-04-27 00:19:19 +0200

    Initial commit

--- source/src/convolutionLayer.java
+++ source/src/convolutionLayer.java
@@ -1,9 +1,12 @@
 public class convolutionLayer {
+    //一个卷积计算就是相当于之前NN中的一个神经元 是卷积运算中的基本单位
     double[][] kernel;
     double bias;
-    convolutionLayer(double[][] k,double b){
+    private activateFuction af;
+    convolutionLayer(double[][] k,double b,activateFuction a){
         kernel=k;
         bias=b;
+        af=a;
     }
     //这里是对传入的二维数据做一个卷积运算
     public double[][] forward(double[][] input){
@@ -12,6 +15,7 @@ public class convolutionLayer {
 
         int kernelR=kernel.length;
         int kernelC=kernel[0].length;
+
         //这里的加一对应的是kernel这个矩阵和初始数据的第一个结果 差值是能走多少步 就会产生多少阶
         int outR=inputRow-kernelR+1;
         int outC=inputColumn-kernelC+1;
@@ -28,7 +32,9 @@ public class convolutionLayer {
                         //对每一个运算后的矩阵上的所有点求和
                     }
                 }
-                output[i][j]=sum+bias;
+                output[i][j]=af.activate(sum+bias);
+                //这里的af和之前在NN中的意义是一样的就是对输出结果进行激活
+                //在卷积中的激活函数是对每一个矩阵中的元素进行激活
             }
         }
         return output;
